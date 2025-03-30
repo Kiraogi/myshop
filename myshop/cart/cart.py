d175088fb9c8008997525605f3e865309fe8a4d7
Kiraogi <golokron3@yandex.ru>
2025-03-30 18:08:18 +0300

    ver 0.01.10
    Add cart app and session ID to settings

---
+++ myshop/cart/cart.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+from django.conf import settings
+
+from shop.models import Product
+
+class Cart:
+    def __init__(self, request):
+        """
+        Инициализировать корзину
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # Сохранить пустую корзину в сеансе
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
