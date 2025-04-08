b6f84758c9cc9b7563cfe766aa8a0fcb951412a2
Kiraogi <golokron3@yandex.ru>
2025-04-08 21:42:44 +0300

    ver 0.01.29
    `Added view and URL for admin order detail, and added link to admin order detail in admin panel`

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,10 +1,11 @@
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
-from cart.cart import Cart
 from .tasks import order_created
+from cart.cart import Cart
 
 def order_create(request):
     cart = Cart(request)
@@ -28,3 +29,8 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
