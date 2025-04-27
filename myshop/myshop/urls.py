884dec6f68be6a5fa8e88f73d9b39412698cf6bb
Kiraogi <golokron3@yandex.ru>
2025-04-27 15:41:00 +0300

    ver 0.01.47
    `Moved Stripe webhook URL from payment/urls.py to myshop/urls.py`

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 
+from payment import webhooks
+
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path(_('cart/'), include('cart.urls', namespace='cart')),
@@ -30,6 +32,10 @@ urlpatterns = i18n_patterns(
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
 )
+
+urlpatterns += [
+    path('payment/webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
